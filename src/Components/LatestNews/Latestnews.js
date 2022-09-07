4c89a110f9b01341a59d7e28c4e806b1a703e3ce
vera zhang <vera.zhang@i4ware.fi>
2022-09-07 21:12:48 +0300

    closure

--- src/Components/LatestNews/Latestnews.js
+++ src/Components/LatestNews/Latestnews.js
@@ -2,14 +2,16 @@ import React,{useEffect,useState} from 'react';
 import styles from './Latestnews.module.css';
 
 function Latestnews() {
- 
-const [date,setDate]=useState("");
-const [description,setDescription]=useState("")
 
+var aa=[]
+var dd=[]
 
   const BASE_URL = "https://www.lianatech.com/resources/blog.rss";
   const CROSS_DOMAIN = 'https://cors-anywhere.herokuapp.com';
   const REQUEST_URL = `${CROSS_DOMAIN}/${BASE_URL}`;
+
+
+
 
 
 
@@ -25,19 +27,31 @@ const [description,setDescription]=useState("")
         )
         .then (
           (data)=>{ 
-            let items=data.getElementsByTagName("item");
-            for(let i=0;i<items.length;i++){
-              let date=items[i].getElementsByTagName("pubDate")[0].textContent;  
-              let  desc=items[i].getElementsByTagName("description")[0].textContent;
-              setDate(date)
-              setDescription(desc)
+
+            function getXMLData(){
+              let items=data.getElementsByTagName("item");    
+              for(let i=0;i<items.length;i++)
+                 return (function (y) { 
+                      let cc=y[0].getElementsByTagName("pubDate")[0].textContent;
+                      let bb=y[0].getElementsByTagName("description")[0].textContent;
+                       aa[i]=cc
+                       dd[i]=bb
+
+                     }
+                  )([items[i]])
+              
+            getXMLData()
             }
-          }
+         
+        }
+           
         )
     },[REQUEST_URL]
   )
+//  let dates=y.getElementsByTagName("pubDate")[0].textContent; 
+// let desc=y.getElementsByTagName("description")[0].textContent;
+console.log(aa,dd)
 
-console.log(date,description)
 
   return (
    
@@ -46,16 +60,16 @@ console.log(date,description)
         <ul className={styles.wrapper}>
         
           <li>
-            <p className={styles.date}>{date}</p>
-            <p className={styles.message}>{description}</p>
+            <p className={styles.date}>{}</p>
+            <p className={styles.message}>{}</p>
           </li>
           <li>
-            <p className={styles.date}>{date}</p>
-            <p className={styles.message}>{description}</p>
+            <p className={styles.date}>{}</p>
+            <p className={styles.message}>{}</p>
           </li>
           <li>
-            <p className={styles.date}>{date}</p>
-            <p className={styles.message}>{description}</p>
+            <p className={styles.date}>{}</p>
+            <p className={styles.message}>{}</p>
           </li>
         </ul>
     </div>
